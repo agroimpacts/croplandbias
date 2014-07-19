697489a50b645855fff80ad3c0087144b2f10421
Lyndon D. Estes <lestes@mapper.Princeton.EDU>
2014-07-18 23:27:21 -0400

    Made more efficient, removed grid overlaps, intersects processed by individual grid cell and its overlapping fields

--- scripts/SAGridFieldsIntersect.R
+++ scripts/SAGridFieldsIntersect.R
@@ -25,6 +25,7 @@ prjstr <- dbGetQuery(con, prj.sql)$proj4text
 fldname <- "gti_sa_2011_alb"
 gridname <- "sa1kilo"
 
+########################################################
 # Grab data list of SA 1 km grids
 grid.sql <- "select id from sa1kilo"
 grids <- dbGetQuery(con, grid.sql)$id
@@ -55,10 +56,11 @@ field.centers <- lapply(1:nrow(f.inds), function(x) {
 
 field.centers.t <- as.data.frame(do.call(rbind, field.centers))
 colnames(field.centers.t) <- c("gid", "x", "y")
-field.centers.to[1:100, ]
 field.centers.to <- field.centers.t[with(field.centers.t, order(y, x, decreasing = TRUE)), ]
-proj4string(field.centers.to) <- prjstr
 coordinates(field.centers.to) <- ~x + y
+proj4string(field.centers.to) <- prjstr
+##########################################
+
 # plot(field.centers.to[1:20000, ], cex = 0.1, pch = 20)
 # plot(field.centers.to)
 # nrow(field.centers.to)
@@ -82,6 +84,156 @@ fld.types <- unname(apply(fld.types, 1, tolower))
 fld.types[grep("subsistence", fld.types)] <- "subsistence"
 fld.types <- unique(fld.types)
 
+sql <- paste("SELECT ST_AsEWKT(ST_SetSRID(ST_Extent(geom),", prjsrid, ")) as table_extent FROM", gridname)
+bound.box <- dbGetQuery(con, sql)
+bound.box <- gsub("^SRID=*.*;", "", bound.box)
+bound.poly <- polyFromWkt(geom.tab=cbind(1, bound.box), crs=prjstr)
+bound.ext <- extent(bound.poly)
+chunk <- 20000
+bound.incr <- (bound.ext@ymax - bound.ext@ymin) / chunk
+bound.rem <- (bound.ext@ymax - bound.ext@ymin) %% chunk
+ys <- seq(bound.ext@ymax, bound.ext@ymin, -chunk)
+ysb <- c(ys, ys[length(ys)] - bound.rem)
+yadd <- cbind(ys[-length(ysb)], ysb[-1])
+
+split.polys <- lapply(1:nrow(yadd), function(x) {
+  coords <- c(xmin = bound.ext@xmin, xmax = bound.ext@xmax, ymin = yadd[x, 2], ymax = yadd[x, 1])
+#   subpoly <- as(extent(coords), "SpatialPolygons")
+})
+
+# plot(bound.poly)
+# for(i in split.polys) plot(i, add = TRUE, border = "transparent", col = "red")
+
+# Pull out SA grid cells corresponding to polygon "chunks"
+check.id.overlaps <- lapply(1:length(split.polys), function(x) {
+  print(x)
+  box.geom <- writeWKT(as(extent(split.polys[[x]]), "SpatialPolygons"))
+  sql <- paste("SELECT ", gridname, ".id FROM ", gridname, " WHERE st_intersects('SRID=", prjsrid, ";", 
+               box.geom, "'::geometry, ", gridname, ".geom)", sep = "")
+  grid.geom.tab <- dbGetQuery(con, sql)$id
+})
+# length(unlist(check.id.overlaps)); length(unique(unlist(check.id.overlaps)))
+# Find and remove overlapping polygons 
+grid.ids <- lapply(2:length(check.id.overlaps), function(x) {
+  check.id.overlaps[[x]][!check.id.overlaps[[x]] %in% check.id.overlaps[[x - 1]]]
+})  
+grid.ids.all <- vector("list", length(check.id.overlaps))
+grid.ids.all[[1]] <- check.id.overlaps[[1]] 
+for(i in 2:length(grid.ids.all)) grid.ids.all[[i]] <- grid.ids[[i - 1]]
+# length(unlist(grid.ids.all))
+#which(duplicated(unlist(grid.ids.all)))  # no duplicates, so theoretically no overlaps left
+
+fld.1km.ints <- lapply(1:length(grid.ids), function(x) {
+  x <- 1
+#   box.ext <- split.polys[[x]]
+#   box.geom <- writeWKT(as(extent(box.ext), "SpatialPolygons"))
+#   sql <- paste("SELECT ", gridname, ".id FROM ", gridname, " WHERE st_intersects('SRID=", prjsrid, ";", 
+#                box.geom, "'::geometry, ", gridname, ".geom)", sep = "")
+#   grid.id.tab <- dbGetQuery(con, sql)$id
+  
+  # select fields intersecting with that area also
+  sql <- paste("SELECT ", fldname, ".gid FROM ", fldname, " WHERE st_intersects('SRID=", prjsrid, ";", 
+               box.geom, "'::geometry, ", fldname, ".geom)", sep = "")
+  fld.id.tab <- dbGetQuery(con, sql)$gid
+  
+    # select fields intersecting with grid ids within the box
+  sql <- paste("SELECT ", fldname, ".gid FROM ", fldname, " WHERE st_intersects('SRID=", prjsrid, ";", 
+               box.geom, "'::geometry, ", fldname, ".geom)", sep = "")
+  fld.id.tab <- dbGetQuery(con, sql)$gid
+  sql <- paste("SELECT ", gridname, ".id, ", fldname, ".gid, ", fldname, ".catname_1 ",  "FROM ", gridname, 
+               " INNER JOIN ", fldname, " on st_intersects(", gridname, ".geom, ", fldname, ".geom) WHERE ", 
+               gridname, ".id in (", paste(grid.ids.all[[x]], collapse = ","), ")", " AND ", fldname, 
+               ".gid in (", paste(fld.id.tab, collapse = ","), ")", sep = "")
+  system.time(fieldgrid.id.tab <- dbGetQuery(con, sql))
+  fieldgrid.id.tab[, 3] <- sapply(1:nrow(fieldgrid.id.tab), function(j) tolower(fieldgrid.id.tab[j, 3]))
+  #   fmatches <- match(geom.tab[, 2], fld.types, nomatch=999)
+  fmatches <- lapply(fld.types, function(y) grep(y, fieldgrid.id.tab[, 3])) 
+  names(fmatches) <- fld.types
+  if(sum(sapply(fmatches, length)) != nrow(fieldgrid.id.tab)) stop("Some field types were missed!")
+  
+  fld.type.ints <- lapply(1:2, function(j) {
+    #     j <- 4
+    print(paste("Processing intersections with Class", j, ":", names(fmatches)[j]))
+    fld.sel <- fieldgrid.id.tab[fmatches[[j]], ]
+    if(nrow(fld.sel) == 0) {
+      fld.ints <- 0
+    } else {
+      sql <- paste("SELECT id, ST_AsText(geom) FROM ", gridname, " WHERE id in (",
+                 paste(fld.sel$id, collapse = ","), ")", sep = "")
+      grid.geom.tab <- dbGetQuery(con, sql)
+      sql <- paste("SELECT gid, ST_AsText(geom) FROM ", fldname, " WHERE gid in (",
+                   paste(fld.sel$gid, collapse = ","), ")", sep = "")
+      fld.geom.tab <- dbGetQuery(con, sql)
+      
+      fld.ints <- t(sapply(unique(fld.sel$id), function(k) {
+        #k <- unique(fld.sel$id)[40]
+        print(paste("processing grid", k))
+        #       sql <- paste("SELECT id, ST_AsText(geom) FROM ", gridname, " WHERE id=", k, sep = "")
+        #       grid.geom.tab <- dbGetQuery(con, sql)
+        #       grid.geom.poly <- polyFromWkt(geom.tab=grid.geom.tab, crs=prjstr)
+        #       sql <- paste("SELECT gid, ST_AsText(geom) FROM ", fldname, " WHERE gid in (",
+        #                    paste(fld.sel[fld.sel$id == k, "gid"], collapse = ","), ")", 
+        #                    sep = "")
+        #       fld.geom.tab <- dbGetQuery(con, sql)
+        #       g <- grid.geom.tab[grid.geom.tab$id == k, ]
+        g <- polyFromWkt(geom.tab=grid.geom.tab[grid.geom.tab$id == k, ], crs=prjstr)
+        fid <- fld.sel[fld.sel$id == k, "gid"]
+        f <- polyFromWkt(geom.tab=fld.geom.tab[fld.geom.tab$gid %in% fid, ], crs=prjstr)
+        #plot(g)
+        #plot(f, add = TRUE)
+        if(!gIsValid(f)) {
+          f.union <- gUnaryUnion(f)
+          if(!gIsValid(f.union)) {
+            print("Unioned geometry not valid, first try fix with gBuffer")
+            f.union2 <- gBuffer(f.union, width=0)
+            if(!gIsValid(f.union2)) {
+              print("Still not valid, call pprepair to clean this one")
+              td <- tempdir()
+              tmpnmin <- strsplit(tempfile("poly", tmpdir = ""), "/")[[1]][2]
+              tmpnmout <- strsplit(tempfile("poly", tmpdir = ""), "/")[[1]][2]
+              writeOGR(f.union2, dsn = td, layer = tmpnmin, driver = "ESRI Shapefile")
+              f.union3 <- callPprepair(td, spdfinname = tmpnmin, spdfoutname = tmpnmout, 
+                                       crs = f.union2@proj4string)
+              f.polys <- f.union3
+            } else {
+              print("Buffering work, geometry now valid")
+              f.polys <- f.union2
+            }
+          } else {
+            print("Unioned geometry is valid")
+            f.polys <- fld.geom.union
+          } 
+        } else {
+          f.polys <- f
+        }
+        #print("Processing intersections now")
+        grid.int <- gIntersection(g, f.polys, byid = TRUE)
+        farea <- gArea(grid.int, byid = TRUE) / 10000
+        c("id" = k, "area" = unname(sum(farea)))  # sum areas to make for single record
+        #plot(g)
+        #plot(grid.int, add = TRUE)
+      }))
+  })
+  
+  
+  
+#   sql <- paste("SELECT ", gridname, ".id, ", fldname, ".gid, ", fldname, ".catname_1 ",  "FROM ", gridname, 
+#                " INNER JOIN ", fldname, " on ", gridname, ".id in (", 
+#                paste(grid.ids.all[[x]][1:200], collapse = ","), ")", " AND ", fldname, 
+#                ".gid in (", paste(fld.id.tab[1:200], collapse = ","), ") AND", 
+#                " st_intersects(", gridname, ".geom, ", fldname, ".geom)", sep = "")
+#   system.time(fieldgrid.id.tab2 <- dbGetQuery(con, sql))
+  
+  
+})
+
+
+
+
+
+
+
+
 # Read in SA grids
 chunk <- 10000
 a <- seq(1, length(grids), chunk)
@@ -92,21 +244,6 @@ g.inds[1] <- b[1]
 fld.1km.ints <- lapply(1, function(x) {
   print(x)
 #   x <- 1
-#   sql <- paste("select id, ST_AsEWKT(geom) from sa1kilo where id>=", g.inds[x, 1], " and id<=", 
-#                g.inds[x, 2], sep = "")
-#   sql <- paste("SELECT id, ST_AsEWKT(ST_Centroid(geom)) as center FROM", gridname,  "where id>=", 
-#                g.inds[x, 1], "and id<=", g.inds[x, 2])
-#   geom.tab <- dbGetQuery(con, sql)
-#   geom.tab[, 2] <- gsub("^SRID=*.*;", "", geom.tab[, 2])
-#   coord.mat <- do.call(rbind, lapply(1:nrow(geom.tab), function(y) {
-#     strip <- gsub("POINT|\\(|\\)", "", geom.tab[y, 2])
-#     coord.mat <- do.call(rbind, strsplit(strsplit(strip, ",")[[1]], " "))
-#   }))
-#   class(coord.mat) <- "numeric"
-#   point.tab <- as.data.frame(cbind(geom.tab[, 1], coord.mat))
-#   colnames(point.tab) <- c("gid", "x", "y")
-#   point.ext <- as(extent(point.tab), "SpatialPolygons")
-#   proj4string(point.ext) <- prjstr
   sql <- paste("SELECT ST_AsEWKT(ST_SetSRID(ST_Extent(geom),", prjsrid, ")) as table_extent FROM", 
                gridname, "where id>=", g.inds[x, 1], "and id<=", g.inds[x, 2])
   bound.box <- dbGetQuery(con, sql)
@@ -130,116 +267,54 @@ fld.1km.ints <- lapply(1, function(x) {
   names(fmatches) <- fld.types
   if(sum(sapply(fmatches, length)) != nrow(fld.type.tab)) stop("Some field types were missed!")
   
-  lapply(fmatches, function(z) {
-#     z <- fmatches[[1]]
-    fld.sel <- fld.type.tab[z, ]
+  
+  fld.type.ints <- lapply(1:2, function(z) {
+#     z <- 2
+    print(paste("Processing intersections with Class", z, ":", names(fmatches)[z]))
+    fld.sel <- fld.type.tab[fmatches[[z]], ]
 #     fld.sel <- fld.sel[1:3, ]
 
     sql <- paste("SELECT ", gridname, ".id, ST_AsEWKT(", gridname, ".geom) FROM ", gridname, " INNER JOIN ", 
                  fldname, " on st_intersects(", gridname, ".geom, ", fldname, ".geom) WHERE ", gridname, 
                  ".id>=", g.inds[x, 1], " AND ", gridname, ".id<=", g.inds[x, 2], " AND ", fldname, 
                  ".gid in (", paste(fld.sel[, 1], collapse = ","), ")", sep = "")
+    print("  fetching intersecting grid geometries")
     grid.geom.tab <- dbGetQuery(con, sql)
     grid.geom.tab[, 2] <- gsub("^SRID=*.*;", "", grid.geom.tab[, 2])
+    print("  creating spatialPolygons for grids")
     grid.geom.poly <- polyFromWkt(geom.tab=grid.geom.tab, crs=prjstr)
     sql <- paste("SELECT gid, ST_AsEWKT(geom) FROM ", fldname, " WHERE gid in (", 
                  paste(fld.sel[, 1], collapse = ","), ")", sep = "")
     fld.geom.tab <- dbGetQuery(con, sql)
     fld.geom.tab[, 2] <- gsub("^SRID=*.*;", "", fld.geom.tab[, 2])
+    print("  creating spatialPolygons for fields")
     fld.geom.poly <- polyFromWkt(geom.tab=fld.geom.tab, crs=prjstr)
     fld.geom.union <- gUnaryUnion(fld.geom.poly)
-    fld.geom.union <- gBuffer(fld.geom.union, width=0)
-    if(gIsValid(fld.geom.union)) {
-      grid.int <- gIntersection(grid.geom.poly, fld.geom.union, byid = TRUE)
-      grid.geom.poly$area <- gArea(grid.int, byid = TRUE) / 10000
+    if(!gIsValid(fld.geom.union)) {
+      print("Unioned geometry not valid, first try fix with gBuffer")
+      fld.geom.union2 <- gBuffer(fld.geom.union, width=0)
+      if(!gIsValid(fld.geom.union2)) {
+        print("Still not valid, call pprepair to clean this one")
+        td <- tempdir()
+        tmpnmin <- strsplit(tempfile("poly", tmpdir = ""), "/")[[1]][2]
+        tmpnmout <- strsplit(tempfile("poly", tmpdir = ""), "/")[[1]][2]
+        writeOGR(fld.geom.union2, dsn = td, layer = tmpnmin, driver = "ESRI Shapefile")
+        fld.geom.union3 <- callPprepair(td, spdfinname = tmpnmin, spdfoutname = tmpnmout, 
+                                        crs = fld.geom.union2@proj4string)
+        fld.polys <- fld.geom.union3
+      } else {
+        print("Buffering work, geometry now valid")
+        fld.polys <- fld.geom.union2
+      }
+    } else {
+      print("Unioned geometry is valid")
+      fld.polys <- fld.geom.union
     }
+    print("Processing intersections now")
+    system.time(grid.int <- gIntersection(grid.geom.poly, fld.polys, byid = TRUE))
+    grid.geom.poly$area <- gArea(grid.int, byid = TRUE) / 10000
   })
 
   fld.types <- unique(fld.types)
   geom.tab[, 3] <- gsub("^SRID=*.*;", "", geom.tab[, 3])
-  
-  
-  
-  
-  fld.types
-  
-  
-  
-  
-  geom.tab[1:10, ]
-  
-  
-  
-  coordinates(field.int) <- ~x + y
-  
-  
-  
-  plot(field.int)
-  plot(point.ext, add = T)
-  
-  colnames(point.tab) <- c("gid", "x", "y")
-  coordinates(point.tab) <- ~x + y
-  proj4string(point.tab) <- prjstr
-  gridded(point.tab) <- TRUE
-  as(point.tab, "raster")
-  plot(point.tab)
-  class(point.tab)
-  r <- raster(point.tab)
-  
-  plot(r)
-  
-  point.tab.t <- as.data.frame(do.call(rbind, field.centers))
-  colnames(field.centers.t) <- c("gid", "x", "y")
-  
-  plot(point.tab, cex = 0.1, pch = 20)
-
-  sql <- paste("select id, ST_AsEWKT(geom) from sa1kilo where id>=", g.inds[x, 1], " and id<=", 
-               g.inds[x, 2], sep = "")
-  geom.tab <- dbGetQuery(con, sql)
-  geom.tab[, 2] <- gsub("^SRID=*.*;", "", geom.tab[, 2])
-  geom.poly <- polyFromWkt(geom.tab=geom.tab, crs=prjstr)
-  plot(geom.poly[1:10, ])
-  plot(point.tab[1:10, ], add = TRUE)
-  plot(r, add = TRUE)
-  
-  test <- over(point.tab, field.centers.to)
-  test <- gIntersects(point.tab, field.centers.to)
-  
-  
-  p1 <- as(extent(geom.poly), "SpatialPolygons")
-  p2 <- as(extent(geom.poly), "SpatialPolygons")
-  plot(p1)
-  plot(p2, add = TRUE)
-  p1@data
-  
-  plot(grids.poly)
-  
-})
-
-
-field.bounds <-lapply(1:nrow(f.inds), function(x) {
-  field.centers <-lapply(1:nrow(f.inds), function(x) {
-    print(x)
-    flds.sql <- paste("select gid, ST_AsEWKT(geom) from ", fldname, " where gid>=", f.inds[x, 1], " and gid<=", 
-                      f.inds[x, 2], sep = "")
-    flds.geom.tab <- dbGetQuery(con, flds.sql)
-    flds.geom.tab[, 2] <- gsub("^SRID=*.*;", "", flds.geom.tab[, 2])
-    #   flds.poly <- polyFromWkt(geom.tab=flds.geom.tab, crs=prjstr)
-    #   coord.mat <- do.call(rbind, lapply(1:nrow(flds.geom.tab), function(x) {
-    #     strip <- gsub("MULTIPOLYGON|\\(|\\)", "", flds.geom.tab[x, 2])
-    #     coord.mat <- do.call(rbind, strsplit(strsplit(strip, ",")[[1]], " "))
-    #   }))
-    coord.mat <- do.call(rbind, lapply(1:nrow(flds.geom.tab), function(x) {
-      strip <- gsub("MULTIPOLYGON|\\(|\\)", "", flds.geom.tab[x, 2])
-      coord.mat <- do.call(rbind, strsplit(strsplit(strip, ",")[[1]], " "))
-      class(coord.mat) <- "numeric"
-      point.mu <- colMeans(coord.mat)
-    }))
-    #   
-    #   class(coord.mat) <- "numeric"
-    #   bounds <- c(apply(coord.mat, 2, min), apply(coord.mat, 2, max))
-    #   bounds <- bounds[c(1, 3, 2, 4)]
-    #   bound.poly <- as(extent(bounds), "SpatialPolygons")
-    #   return(bound.poly)
-  })
   
