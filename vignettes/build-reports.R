bc2b8dc3a4ec8d40271da9d2cac697343d93e291
ldemaz <lestes@princeton.edu>
2014-11-14 20:01:08 -0500

    Created build-reports.R file to process Rmd files and save html files in vignettes folder.

---
+++ vignettes/build-reports.R
@@ -0,0 +1,14 @@
+# build-reports.R
+# Builds output Rmd files
+
+library(lmisc)
+require(knitr)
+require(markdown)
+
+p_root <- proj_root("SAcropland")
+p_vignettes <- full_path(p_root, "SAcropland/vignettes")
+
+# Create .md, .html, and .pdf files
+knit("vignettes/compare-landcover.Rmd", output = "vignettes/compare-landcover.md")
+markdownToHTML("vignettes/compare-landcover.md", "vignettes/compare-landcover.html", options=c("use_xhml"))
+
